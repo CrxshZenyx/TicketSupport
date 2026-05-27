f54acb344693cb1d611787f7c1be0266572a2337
CrxshZenyx <zenyx972@gmail.com>
2026-05-27 18:52:17 -0400

    Implement add command for ticket permissions

--- commands/add.js
+++ commands/add.js
@@ -1,1 +1,29 @@
+module.exports = {
+  name: "add",
 
+  run: async (client, message, args) => {
+
+    if (!message.channel.name.startsWith("ticket")) return;
+
+    const user = message.mentions.users.first();
+    const role = message.mentions.roles.first();
+
+    if (user) {
+      message.channel.permissionOverwrites.edit(user.id, {
+        ViewChannel: true,
+        SendMessages: true
+      });
+      return message.reply("User added to ticket.");
+    }
+
+    if (role) {
+      message.channel.permissionOverwrites.edit(role.id, {
+        ViewChannel: true,
+        SendMessages: true
+      });
+      return message.reply("Role added to ticket.");
+    }
+
+    message.reply("You must mention a user or role.");
+  }
+};
