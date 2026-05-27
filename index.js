a7d6764bde9c7b68e714c59b1fd58d51325ea80f
CrxshZenyx <zenyx972@gmail.com>
2026-05-27 18:48:25 -0400

    Initialize Discord bot with command loading

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
+const { Client, GatewayIntentBits, Partials, Collection } = require("discord.js");
+const config = require("./config.json");
+const fs = require("fs");
 
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildMembers
+  ],
+  partials: [Partials.Channel]
+});
+
+client.commands = new Collection();
+
+// Load commands
+for (const file of fs.readdirSync("./commands")) {
+  const command = require(`./commands/${file}`);
+  client.commands.set(command.name, command);
+}
+
+// Events
+require("./events/messageCreate")(client);
+require("./events/interactionCreate")(client);
+
+client.login(config.token);
